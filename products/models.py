5debf5b7bb069a5bce204d32961eb6ad260763a2
yeonjae <pyj3740@gmail.com>
2024-09-05 21:32:23 +0900

    black

--- products/models.py
+++ products/models.py
@@ -13,3 +13,11 @@ class Product(models.Model):
     author = models.ForeignKey(get_user_model(), related_name="product", on_delete=models.CASCADE)
 
     like_users = models.ManyToManyField(get_user_model(), related_name='like_products')
+    tags = models.ManyToManyField("products.Hashtag", verbose_name="해시태그 목록", blank=True, related_name='product_tags')
+
+
+class Hashtag(models.Model):
+    tag = models.CharField("tag", max_length=20)
+
+    def __str__(self):
+        return self.tag
