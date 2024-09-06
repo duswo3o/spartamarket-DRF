4a15a85a93fbba01768b5a5e13c5224d8fe5d247
yeonjae <pyj3740@gmail.com>
2024-09-06 09:28:12 +0900

    product 모델에 __str__추가

--- products/models.py
+++ products/models.py
@@ -5,15 +5,25 @@ from django.contrib.auth import get_user_model
 class Product(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
-    image = models.ImageField(upload_to='images/', blank=True)
+    image = models.ImageField(upload_to="images/", blank=True)
 
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
 
-    author = models.ForeignKey(get_user_model(), related_name="product", on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        get_user_model(), related_name="product", on_delete=models.CASCADE
+    )
 
-    like_users = models.ManyToManyField(get_user_model(), related_name='like_products')
-    tags = models.ManyToManyField("products.Hashtag", verbose_name="해시태그 목록", blank=True, related_name='product_tags')
+    like_users = models.ManyToManyField(get_user_model(), related_name="like_products")
+    tags = models.ManyToManyField(
+        "products.Hashtag",
+        verbose_name="해시태그 목록",
+        blank=True,
+        related_name="product_tags",
+    )
+
+    def __str__(self):
+        return self.title
 
 
 class Hashtag(models.Model):
